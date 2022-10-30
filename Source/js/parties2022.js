ab536d5f75f228e5c997d4608b6060d3f3dc8904
gngj <shmueljacobs@gmail.com>
2022-10-30 20:14:07 +0200

    Fix num1

--- Source/js/parties2022.js
+++ Source/js/parties2022.js
@@ -1,18 +1,18 @@
 var getParties = function () {
 
   var likud = new party('ליכוד', 1066669);
-  var yeshatid = new party('יש עתיד', 613810);
-  var shas = new party('שס', 315953);
-  var kachollavan = new party('המחנה הממלכתי', 291815);
+  var yeshatid = new party('יש עתיד', 875959);
+  var shas = new party('שס', 319637);
+  var kachollavan = new party('המחנה הממלכתי', 455498);
   var yamina = new party('הבית היהודי', 0);
-  var avoda = new party('עבודה', 268687);
-  var yahadut = new party('יהדות התורה', 248605);
-  var betenu = new party('ישראל ביתנו', 248340);
-  var tziyonutdatit = new party('הציונות הדתית', 225539);
-  var meshutefet = new party('חד"ש תע"ל', 212188);
-  var meretz = new party('מרצ', 202153);
+  var avoda = new party('עבודה', 165307);
+  var yahadut = new party('יהדות התורה', 226601);
+  var betenu = new party('ישראל ביתנו', 204031);
+  var tziyonutdatit = new party('הציונות הדתית', 489378);
+  var meshutefet = new party('חד"ש תע"ל', 175190);
+  var meretz = new party('מרצ', 198366);
   var aravitmeuhedet = new party('ערבית מאוחדת', 167028);
-  var balad = new party('בל"ד', 212188);
+  var balad = new party('בל"ד', 0);
 
   var parties = [
     likud,
